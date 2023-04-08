8d586bb9d2634dc66301698620bf2fa1b077a288
Luis Wolf <development@pawcode.de>
2023-04-08 15:27:29 +0200

    Update Angular 15.2.6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./pages/home/home.component";
 import {NgModule} from "@angular/core";
-import {PaletteGuard} from "./guards/palette.guard";
+import {canEditPalette} from "./guards/palette.guard";
 import {EditComponent} from "./pages/edit/edit.component";
 import {PreviewComponent} from "./pages/preview/preview.component";
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HomeComponent },
-  { path: 'edit', pathMatch: 'full', canActivate: [PaletteGuard], component: EditComponent },
-  { path: 'preview', pathMatch: 'full', canActivate: [PaletteGuard], component: PreviewComponent }
+  {path: '', pathMatch: 'full', component: HomeComponent},
+  {path: 'edit', pathMatch: 'full', canActivate: [canEditPalette], component: EditComponent},
+  {path: 'preview', pathMatch: 'full', canActivate: [canEditPalette], component: PreviewComponent},
+  {path: '**', redirectTo: ''}
 ]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
